d25ac64dffc71d562f67c641116a0d9c67fcacd3
Marcos Ferreira <marcosprofissional15@gmail.com>
2025-07-08 13:34:55 -0300

    Desafio Concluído

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Console.WriteLine("Criando Iphone");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando App - {nome}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia Nok1 = new Nokia("123456", "Nokia-S", "C4:G4:B4:C6", 500);
+Iphone Iphone1 = new Iphone("123477", "Iphone-XS", "C7:G7:B7:C7", 500);
+
+Nok1.Ligar();
+Iphone1.Ligar(); 
+Nok1.ReceberLigacao();
+Iphone1.ReceberLigacao();
+Nok1.InstalarAplicativo("NETFLIX APP");
+Iphone1.InstalarAplicativo("APPLE APP");
